0526f12c44727d7ee6f4a96bbd162eab0b0212b9
abk <abiekidane19@hotmail.com>
2022-03-13 17:15:40 +0100

    sql config file

--- backend/src/main/resources/db/migration/V1.sql
+++ backend/src/main/resources/db/migration/V1.sql
@@ -11,6 +11,8 @@ CREATE TABLE IF NOT  EXISTS ecomm.cart (
 CREATE TABLE IF NOT EXISTS ecomm.cart.item(
     cart_id uuid NOT null,
     item_id uuid NOT null,
+    foreign key (cart_id) references ecomm.cart
+    foreign  key (item_id) references ecomm.Item
 
 )
 CREATE TABLE ID NOT EXISTS ecomm.Item (

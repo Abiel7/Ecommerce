1195d202b353c664aec054f9d410365df161e778
abk <abiekidane19@hotmail.com>
2022-03-17 18:57:22 +0100

    more end points and set up cart Controller for testing

--- backend/src/main/java/com/ecomm/api/backend/controller/CartController.java
+++ backend/src/main/java/com/ecomm/api/backend/controller/CartController.java
@@ -1,9 +1,13 @@
 package com.ecomm.api.backend.controller;
 
 
+import com.ecommerce.api.CartApi;
+import com.ecommerce.api.model.Cart;
+import com.ecommerce.api.model.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -11,17 +15,49 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
+@RestController
+public class CartController implements CartApi {
 
-public class CartController  {
-   /* private static final Logger log = LoggerFactory.getLogger(CartController.class);
+     private static final Logger log = LoggerFactory.getLogger(CartController.class);
+
 
     @Override
-    public ResponseEntity<List<Item>> addCartItemsByCustomerId(String customerId, @Valid Item item){
+    public ResponseEntity<List<Item>> addItemsOnCartByCustomerId(String customerId, Item item) {
         log.info("Request for customre if {}\nItem: {} ",customerId,item);
 
         return ok(Collections.EMPTY_LIST);
+
     }
 
-    */
+    @Override
+    public ResponseEntity<List<Item>> addOrReplaceItemsByCustomerId(String customerId, Item item) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCart(String customerId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Cart>> getCartByCustomerId(String customerId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Item>> getCartItemById(String customerId, String itemId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Item>> getCartItemsByCutomerId(String customerId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Item>> removeItemsById(String customerId, String itemId) {
+        return null;
+    }
+
 }
 

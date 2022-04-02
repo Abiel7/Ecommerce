a3a46db6899b3e765c13c4ef01dc65356ad931ca
abk <abiekidane19@hotmail.com>
2022-04-03 00:14:00 +0200

    shipment and tag entity modified

--- backend/src/main/java/com/ecomm/api/backend/entity/ShipmentEntity.java
+++ backend/src/main/java/com/ecomm/api/backend/entity/ShipmentEntity.java
@@ -1,10 +1,48 @@
 package com.ecomm.api.backend.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @Entity
 @Table(name = "shipment")
 public class ShipmentEntity {
-    
+
+    @Id
+    @GeneratedValue
+    @Column(name= "ID", nullable = false, updatable = false)
+    private UUID id;
+    @Column(name ="ESTIMATED_DELIVERY_DATE")
+    private Timestamp estimatedDeliveryDate;
+    @Column(name = "CARRIER")
+    private String carrier;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public ShipmentEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public Timestamp getEstimatedDeliveryDate() {
+        return estimatedDeliveryDate;
+    }
+
+    public ShipmentEntity setEstimatedDeliveryDate(Timestamp estimatedDeliveryDate) {
+        this.estimatedDeliveryDate = estimatedDeliveryDate;
+        return this;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public ShipmentEntity setCarrier(String carrier) {
+        this.carrier = carrier;
+        return this;
+    }
+
+
 }
--- backend/src/main/java/com/ecomm/api/backend/entity/TagEntity.java
+++ backend/src/main/java/com/ecomm/api/backend/entity/TagEntity.java
@@ -1,9 +1,43 @@
 package com.ecomm.api.backend.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
 
 @Entity
 @Table(name = "tag")
 public class TagEntity {
+    @Id
+    @GeneratedValue
+    @Column(name="ID", nullable = false,updatable = false)
+    private UUID id;
+    @NotNull(message = "Tag name cannot be null")
+    @Basic(optional = false)
+    @Column(name ="NAME")
+    private  String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TagEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TagEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+
 }
+
+
+
+
+
+

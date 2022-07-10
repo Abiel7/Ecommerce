6fe45bafc0a7c4c2748ad5dc513d57d98ce00120
abk <abiekidane19@hotmail.com>
2022-07-10 21:30:19 +0200

    error hadnling

--- backend/src/main/java/com/ecomm/api/backend/exceptions/ApiErrorWebExceptionHandler.java
+++ backend/src/main/java/com/ecomm/api/backend/exceptions/ApiErrorWebExceptionHandler.java
@@ -1,17 +1,41 @@
 package com.ecomm.api.backend.exceptions;
 
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import reactor.core.publisher.Mono;
 
-public class ApiErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+import java.util.Objects;
 
-   public ApiErrorWebExceptionHandler(ApiError)
+@Component
+@Order(-2) //prioritization, with the first object (with the lowest order value) having the highest priority.
+public class ApiErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler  {
+
+        public ApiErrorWebExceptionHandler(ApiErrorsAttributes errorsAttributes,
+                                           ApplicationContext applicationContext,
+                                           ServerCodecConfigurer serverCodecConfigurer){
+            super(errorsAttributes, new WebProperties().getResources(),applicationContext);
+            super.setMessageWriters(serverCodecConfigurer.getWriters());
+            super.setMessageReaders(serverCodecConfigurer.getReaders());
+
+        }
+
     @Override
     protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
-        return null;
+        return RouterFunctions.route(RequestPredicates.all(),this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse (ServerRequest serverRequest) {
+        Map<String, Object> errorAttributes= getErrorAttributes(serverRequest, ErrorAttributeOptions.defaults());
+         return  null;
     }
 }
